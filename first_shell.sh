d33e3d3b52a5ec34204fbddd4592ab67dc9d2f51
chenluolin <917988385@qq.com>
2021-04-28 16:26:15 +0800

    feat: 第一次提交

---
+++ first_shell.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+
+/*
+#--------------------
+# author：chenluolin
+# time  ：2021.04.28
+# describe：this is first shell
+#
+#--------------------
+*/
+console.log("hoole world!")
